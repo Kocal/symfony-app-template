37e2089fdb38cfec9dfac7cd0cda3b85c63bfeca
Hugo Alliaume <kocal@live.fr>
2019-06-16 15:32:36 +0200

    Install PHPSpec

---
+++ spec/App/AcmeSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\App;
+
+use App\Acme;
+use PhpSpec\ObjectBehavior;
+
+class AcmeSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Acme::class);
+    }
+}
