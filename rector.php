419bd895789b2748b4683805a85783a5e9091b79
Hugo Alliaume <hugo@alliau.me>
2021-05-26 13:51:27 +0200

    chore: configure Rector for Doctrine and PHPUnit

--- rector.php
+++ rector.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -23,10 +25,22 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // register a single rule
     // $services->set(TypedPropertyRector::class);
 
+    // Symfony
     $containerConfigurator->import(SymfonySetList::SYMFONY_52);
     $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
     $parameters->set(
         Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER,
-        __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml'
+        __DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml'
     );
+
+    // Doctrine
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+
+    // PHPUnit
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_90);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_MOCK);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
 };
